2d57d827ab1cd71028d84521fc17c57b300712c0
arthursiq5 <arthur.siq5@gmail.com>
2022-02-28 23:32:49 -0300

    Iniciado desenvolvimento do código

---
+++ main.js
@@ -0,0 +1,34 @@
+let order = []
+let clickorder = []
+let score = 0
+
+// 0 - verde
+// 1 - vermelho
+// 2 - amarelo
+// 3 - azul
+
+const blue = document.querySelector('.azul')
+const red = document.querySelector('.vermelho')
+const green = document.querySelector('.verde')
+const yellow = document.querySelector('.amarelo')
+
+const shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random() * 4)
+    order[order.length] = colorOrder
+    clickedOrder = []
+
+    for (let i in order) {
+        let elementColor = createColorElement(order[i])
+        lightColor(elementColor, Number(i + 1))
+    }
+}
+
+const lightColor = (element, number) => {
+    let number = number + 500
+    setTimeout(() => {
+        element.classList.add('selected')
+    }, number - 250)
+    setTimeout(() => {
+        element.classList.remove('selected')
+    }, number)
+}
